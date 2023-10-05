c1ac870059c8f0db0b099c030003bd1b430a2cc0
jvsch <j.v.schneider@web.de>
2023-10-05 20:12:15 +0200

    using hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,90 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import CardList from "./CardList";
 import Search from "../components/Search";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-  componentDidMount() {
-    this.setState({
-      robots: [
-        {
-          name: "Bob",
-        },
-        {
-          name: "Tom",
-        },
-        {
-          name: "Nick",
-        },
-        {
-          name: "Cevin",
-        },
-        {
-          name: "Robin",
-        },
-        {
-          name: "Cenk",
-        },
-        {
-          name: "Nils",
-        },
-        {
-          name: "David",
-        },
-        {
-          name: "Elia",
-        },
-        {
-          name: "Lino",
-        },
-        {
-          name: "Basti",
-        },
-        {
-          name: "Sonja",
-        },
-        {
-          name: "Jakob",
-        },
-        {
-          name: "Louis",
-        },
-        {
-          name: "Marisa",
-        },
-        {
-          name: "Sarah",
-        },
-        {
-          name: "Ben",
-        },
-        {
-          name: "Niclas",
-        },
-      ],
-    });
-  }
+  useEffect(() => {
+    setRobots([
+      {
+        name: "Bob",
+      },
+      {
+        name: "Tom",
+      },
+      {
+        name: "Nick",
+      },
+      {
+        name: "Cevin",
+      },
+      {
+        name: "Robin",
+      },
+      {
+        name: "Cenk",
+      },
+      {
+        name: "Nils",
+      },
+      {
+        name: "David",
+      },
+      {
+        name: "Elia",
+      },
+      {
+        name: "Lino",
+      },
+      {
+        name: "Basti",
+      },
+      {
+        name: "Sonja",
+      },
+      {
+        name: "Jakob",
+      },
+      {
+        name: "Louis",
+      },
+      {
+        name: "Marisa",
+      },
+      {
+        name: "Sarah",
+      },
+      {
+        name: "Ben",
+      },
+      {
+        name: "Niclas",
+      },
+    ]);
+  }, []);
 
-  onSearch = (event) => {
-    this.setState({ searchfield: event.target.value });
+  const onSearch = (event) => {
+    setSearchfield(event.target.value);
   };
 
-  render() {
-    return (
-      <div id="app-container">
-        <Search handleChange={this.onSearch} />
-        <CardList input={this.state.searchfield} robots={this.state.robots} />
-      </div>
-    );
-  }
+  return (
+    <div id="app-container">
+      <Search handleChange={onSearch} />
+      <CardList input={searchfield} robots={robots} />
+    </div>
+  );
 }
 
 export default App;
